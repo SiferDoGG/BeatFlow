5670c6064e6b13e77bd19ad3f0086aeab7a05069
Roman Korchagin <sifer.roman@mail.ru>
2026-04-24 14:41:54 +0700

    remove any folder from tracking

--- any/Experiment/Yandex-controller/adapters.py
+++
@@ -1,6 +0,0 @@
-class MainMusicAdapter():
-    def play():
-        def wrapper():
-            "Здесь выполняется соединение с content.js для yandex, и остальных сервисов"
-            pass
-        return "ответ для UI"
--- any/trash/g/popup.js
+++
@@ -1,5 +0,0 @@
-function send(type, extra = {}) {
-    fetch("http://127.0.0.1:8000/" + type.toLowerCase(), {
-        method: "POST"
-    });
-}
